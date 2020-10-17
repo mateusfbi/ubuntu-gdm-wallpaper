61d18e9c7c6a19eeb02539ee94b8760b7c67db6d
Mateus Brandão <mateusfbi@gmail.com>
2020-10-17 14:18:25 -0300

    Update set-gdm-wallpaper.sh
    Alteração do nome dos arquivos gnome-shell-theme..gresource para gdm3-theme.gresource

--- set-gdm-wallpaper.sh
+++ set-gdm-wallpaper.sh
@@ -32,10 +32,10 @@ fi
 if [ "$1" = "--uninstall" ]; then
   # Restore file if current gresource file is modified by this script.
   # If wallpaper-gdm.png text inside gresource file, then this is modified file.
-  if grep -q "wallpaper-gdm.png" /usr/share/gnome-shell/gnome-shell-theme.gresource; then
-    cp -f /usr/share/gnome-shell/gnome-shell-theme.gresource.backup /usr/share/gnome-shell/gnome-shell-theme.gresource
+  if grep -q "wallpaper-gdm.png" /usr/share/gnome-shell/gdm3-theme.gresource; then
+    cp -f /usr/share/gnome-shell/gdm3-theme.gresource.backup /usr/share/gnome-shell/gdm3-theme.gresource
 
-    echo 'gnome-shell-theme.gresource recovered'
+    echo 'gdm3-theme.gresource recovered'
   fi
 
   exit
@@ -78,36 +78,36 @@ fi
 echo "Updating wallpaper..."
 
 # Restore gresource from backup if current gresource is modified
-if grep -q "wallpaper-gdm.png" /usr/share/gnome-shell/gnome-shell-theme.gresource; then
-  cp -f /usr/share/gnome-shell/gnome-shell-theme.gresource.backup /usr/share/gnome-shell/gnome-shell-theme.gresource
+if grep -q "wallpaper-gdm.png" /usr/share/gnome-shell/gdm3-theme.gresource; then
+  cp -f /usr/share/gnome-shell/gdm3-theme.gresource.backup /usr/share/gnome-shell/gdm3-theme.gresource
 fi
 
 workdir=$(mktemp -d)
 cd "$workdir"
 
-# Creating gnome-shell-theme.gresource.xml with theme file list and add header
-echo '<?xml version="1.0" encoding="UTF-8"?>' >"$workdir/gnome-shell-theme.gresource.xml"
-echo '<gresources><gresource>' >>"$workdir/gnome-shell-theme.gresource.xml"
+# Creating gdm3-theme.gresource.xml with theme file list and add header
+echo '<?xml version="1.0" encoding="UTF-8"?>' >"$workdir/gdm3-theme.gresource.xml"
+echo '<gresources><gresource>' >>"$workdir/gdm3-theme.gresource.xml"
 
-for res_file in $(gresource list /usr/share/gnome-shell/gnome-shell-theme.gresource); do
+for res_file in $(gresource list /usr/share/gnome-shell/gdm3-theme.gresource); do
   # create dir for theme file inside temp dir
   mkdir -p "$(dirname "$workdir$res_file")"
 
   if [ "$res_file" != "/org/gnome/shell/theme/wallpaper-gdm.png" ]; then
     # extract file ($res_file) from current theme and write it to temp dir ($workdir)
-    gresource extract /usr/share/gnome-shell/gnome-shell-theme.gresource "$res_file" >"$workdir$res_file"
+    gresource extract /usr/share/gnome-shell/gdm3-theme.gresource "$res_file" >"$workdir$res_file"
 
-    # add extracted file name to gnome-shell-theme.gresource.xml
-    echo "<file>${res_file#\/}</file>" >>"$workdir/gnome-shell-theme.gresource.xml"
+    # add extracted file name to gdm3-theme.gresource.xml
+    echo "<file>${res_file#\/}</file>" >>"$workdir/gdm3-theme.gresource.xml"
   fi
 done
 
 # add our image ($image) to theme path and to xml file
-echo "<file>org/gnome/shell/theme/wallpaper-gdm.png</file>" >>"$workdir/gnome-shell-theme.gresource.xml"
+echo "<file>org/gnome/shell/theme/wallpaper-gdm.png</file>" >>"$workdir/gdm3-theme.gresource.xml"
 cp -f "$image" "$workdir/org/gnome/shell/theme/wallpaper-gdm.png"
 
 # add footer to xml file
-echo '</gresource></gresources>' >>"$workdir/gnome-shell-theme.gresource.xml"
+echo '</gresource></gresources>' >>"$workdir/gdm3-theme.gresource.xml"
 
 
 # find #lockDialogGroup block inside gnome-shell.css and replace with new_theme_params with our image
@@ -115,22 +115,22 @@ echo '</gresource></gresources>' >>"$workdir/gnome-shell-theme.gresource.xml"
 new_theme_params="background: #2e3436 url(resource:\/\/\/org\/gnome\/shell\/theme\/wallpaper-gdm.png);$image_parameters"
 sed -i -z -E "s/#lockDialogGroup \{[^}]+/#lockDialogGroup \{$new_theme_params/g" "$workdir/org/gnome/shell/theme/gnome-shell.css"
 
-# create gresource file with file list inside gnome-shell-theme.gresource.xml
-glib-compile-resources "$workdir/gnome-shell-theme.gresource.xml"
+# create gresource file with file list inside gdm3-theme.gresource.xml
+glib-compile-resources "$workdir/gdm3-theme.gresource.xml"
 
 # Do backup only for original gresource file, not modified by this script.
 # If wallpaper-gdm.png text inside gresource file, then this is modified file.
-if ! grep -q "wallpaper-gdm.png" /usr/share/gnome-shell/gnome-shell-theme.gresource; then
-  cp -f /usr/share/gnome-shell/gnome-shell-theme.gresource /usr/share/gnome-shell/gnome-shell-theme.gresource.backup
+if ! grep -q "wallpaper-gdm.png" /usr/share/gnome-shell/gdm3-theme.gresource; then
+  cp -f /usr/share/gnome-shell/gdm3-theme.gresource /usr/share/gnome-shell/gdm3-theme.gresource.backup
   echo "Backup"
 fi
 
-cp -f "$workdir/gnome-shell-theme.gresource" /usr/share/gnome-shell/
+cp -f "$workdir/gdm3-theme.gresource" /usr/share/gnome-shell/
 
 # Strange but safe from bug
 rm -rf "$workdir/org"
-rm -f "$workdir/gnome-shell-theme.gresource.xml"
-rm -f "$workdir/gnome-shell-theme.gresource"
+rm -f "$workdir/gdm3-theme.gresource.xml"
+rm -f "$workdir/gdm3-theme.gresource"
 rm -r "$workdir"
 
 echo "Done!"
